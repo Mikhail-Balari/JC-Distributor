6918fe6632ab2459008f59b8d9c16310b06b2fdc
Mikhail-Balari <balarimikhail@gmail.com>
2023-05-01 20:51:14 -0300

    Se crea componente ItemDetailContainer e ItemDetail

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemCount from './components/ItemCount/ItemCount';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={'Bienvenidos a JC DISTRIBUIDORA'}/>
-      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad Agregada", quantity)}/>
+      <ItemDetailContainer />
     </div>
   );
 }
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,31 @@
+import './assets/ItemDetailContainer.css'
+import { useEffect, useState } from 'react'
+import { getProductById } from '../../asyncMock'
+import ItemDetail from '../ItemDetail/ItemDetail'
+
+
+
+
+
+const ItemDetailContainer = () => {
+    
+    const [product, setProduct] = useState(null)
+
+    useEffect(() => {
+        getProductById('1')
+            .then(response => {
+                setProduct(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+
+    return(
+        <div className='ItemDetailContainer'>
+            <ItemDetail {...product} />
+        </div>
+    )
+}
+
+export default ItemDetailContainer
