7612a7e323e9eb6de3dfd4a9136c8a1f87d70b24
Mikhail-Balari <balarimikhail@gmail.com>
2023-07-08 12:07:31 -0300

    Se logra conectar a Firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -15,7 +15,7 @@ const ItemDetailContainer = () => {
     useEffect(() => {
         setLoading(true)
 
-        const docRef = doc(db, 'products', itemId)
+        const docRef = doc(db, 'Products', itemId)
 
         getDoc(docRef)
             .then(response => {
